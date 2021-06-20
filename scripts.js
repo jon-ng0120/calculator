e95e0436be057bda0f75bc433455f56cc72af15b
Jon Ng <jonathanng15@gmail.com>
2021-06-20 01:51:01 -0700

    Fix bug where second values could not be appended to input container

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const operatorBtns = document.querySelectorAll('.operator');
 let firstVal;
 let secondVal;
 let currentOperator;
+let lastInput;
 
 function add(num1, num2) {
 	return num1 + num2;
@@ -19,33 +20,42 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 == 0) return 'Cannot divide by zero';
 	return num1 / num2;
 }
 
 function operate(nums, operation) {
     const [num1, num2] = nums.map(Number)
-    const result = window[operation](num1, num2)
+    const result = window[operation.getAttribute('data-operator')](num1, num2)
     inputContainer.value = result
     firstVal = result;
     secondVal = ''
 }
 
 function displayValues(btn) {
-    if (currentOperator !== undefined) inputContainer.value = ''
+    if (Array.from(operatorBtns).some(element => element.classList.contains('btns-active'))) {
+        inputContainer.value = ''
+        currentOperator.classList.toggle('btns-active')
+    } 
     inputContainer.value += btn
 }
+
 
 numberBtns.forEach(element => element.addEventListener('click', function() {displayValues(this.textContent)}));
 
 operatorBtns.forEach(element => element.addEventListener('click', function() {
+    if (inputContainer.value !== '') {
+        this.classList.toggle('btns-active')
         if (firstVal == undefined) {
             firstVal = inputContainer.value
         } else {
-            secondVal = inputContainer.value           
+            secondVal = inputContainer.value         
             if ((firstVal !== undefined) && (secondVal !== undefined) ) {
                 operate([firstVal, secondVal], currentOperator) 
             } 
-        } 
-        currentOperator = this.getAttribute('data-operator');
+        }
+        if (this.textContent !== '=') currentOperator = this;
+    }
+        
 }))
 
