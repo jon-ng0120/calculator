c34ed1f2804dd0f3c1ca48cfa489d20f36534178
Jon Ng <jonathanng15@gmail.com>
2021-06-23 01:04:35 -0700

    Changed logic and added styling

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
+'use strict'
 const inputContainer = document.querySelector('.input-container input');
 const resultsContainer = document.querySelector('.results-container input');
+const previewBtns = document.querySelectorAll('.number, .operator')
 const numberBtns = document.querySelectorAll('.number');
-const operatorBtns = document.querySelectorAll('.operator');
-let firstVal;
-let secondVal;
-let currentOperator;
-let lastInput;
+const operatorBtns = document.querySelectorAll('.operator')
+const operatorBtnsContent = Array.from(operatorBtns).map(element => element.textContent);
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete')
+const negativeBtn = document.querySelector('.negative')
+let firstVal = '';
+let secondVal = '';
+let currentOperator = ''
 
 function add(num1, num2) {
 	return num1 + num2;
@@ -24,38 +29,79 @@ function divide(num1, num2) {
 	return num1 / num2;
 }
 
+function equals() {
+    if (firstVal !== '') resultsContainer.value = firstVal
+}
+
 function operate(nums, operation) {
     const [num1, num2] = nums.map(Number)
-    const result = window[operation.getAttribute('data-operator')](num1, num2)
-    inputContainer.value = result
+    const result = window[operation](num1, num2) 
     firstVal = result;
-    secondVal = ''
+    console.log(result);
 }
 
-function displayValues(btn) {
-    if (Array.from(operatorBtns).some(element => element.classList.contains('btns-active'))) {
-        inputContainer.value = ''
-        currentOperator.classList.toggle('btns-active')
-    } 
-    inputContainer.value += btn
-}
-
-
-numberBtns.forEach(element => element.addEventListener('click', function() {displayValues(this.textContent)}));
-
-operatorBtns.forEach(element => element.addEventListener('click', function() {
-    if (inputContainer.value !== '') {
-        this.classList.toggle('btns-active')
-        if (firstVal == undefined) {
-            firstVal = inputContainer.value
-        } else {
-            secondVal = inputContainer.value         
-            if ((firstVal !== undefined) && (secondVal !== undefined) ) {
-                operate([firstVal, secondVal], currentOperator) 
-            } 
-        }
-        if (this.textContent !== '=') currentOperator = this;
+function displayNumber(num) {
+    if (inputContainer.value.slice(-1) == '=') {
+        clear()
     }
-        
+
+    if (firstVal == '' && resultsContainer.value == '0') resultsContainer.value = ''
+    if (operatorBtnsContent.includes(num)) {
+        inputContainer.value += resultsContainer.value + num
+    } else {
+        resultsContainer.value += num
+    } 
+}
+
+function negative() {
+    resultsContainer.value *= -1
+}
+
+function clear() {
+    firstVal = 0;
+    secondVal = 0;
+    firstVal = '';
+    secondVal = '';
+    currentOperator = '';
+    inputContainer.value = '';
+    resultsContainer.value = '0'
+}
+
+function del() {
+    resultsContainer.value = resultsContainer.value.slice(0, -1)
+}
+
+negativeBtn.addEventListener('click', function() {negative()})
+
+deleteBtn.addEventListener('click', function() {del()})
+
+clearBtn.addEventListener('click', function() {clear()})
+
+numberBtns.forEach(element => element.addEventListener('click', function() {
+        displayNumber(this.textContent) 
 }))
 
+
+operatorBtns.forEach(element => element.addEventListener('click', function() {
+    // if (resultsContainer.value !== '0') {
+        if (firstVal == '') {
+            firstVal = resultsContainer.value;
+            displayNumber(this.textContent)
+            resultsContainer.value = '';
+            currentOperator = this.getAttribute('data-operator')
+        } else {
+            secondVal = resultsContainer.value
+            displayNumber(this.textContent)
+            resultsContainer.value = '';
+
+            if ((firstVal !== '') && (secondVal !== '') ) {
+                operate([firstVal, secondVal], currentOperator) 
+            }
+            currentOperator = this.getAttribute('data-operator')       
+    // }
+}
+    
+}))
+document.querySelector('.equals').addEventListener('click', function() {
+    equals()
+})
